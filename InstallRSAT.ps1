487d2b6f040537ceb1a1c66c44b8bc6ad686d57a
jstack01 <45379549+jstack01@users.noreply.github.com>
2023-08-08 22:13:54 -0400

    Create InstallRSAT.ps1

---
+++ InstallRSAT.ps1
@@ -0,0 +1,14 @@
+Write-Host 'Setting UseWUServer Registry to 0...'
+Set-Itemproperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU' -Name 'UseWUServer' -value '0'
+
+Write-Host 'Restarting Windows Update Servive...'
+Restart-Service -Name 'wuauserv'
+
+Write-Host 'Installing RSAT...'
+DISM /online /Add-Capability /CapabilityName:Rsat.ActiveDirectory.DS-LDS.Tools~~~~0.0.1.0
+
+Write-Host 'Setting UseWUServer Registry back to 1...'
+Set-Itemproperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU' -Name 'UseWUServer' -value '1'
+
+Write-Host 'Restarting Windows Update Servive...'
+Restart-Service -Name 'wuauserv'
