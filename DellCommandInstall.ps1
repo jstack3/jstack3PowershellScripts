214e1adc2aed561b6fbabef8b9dbb92af7d826d5
jstack01 <45379549+jstack01@users.noreply.github.com>
2023-11-11 12:10:55 -0500

    Update DellCommandInstall.ps1
    Attempting to fix first line corruption.

--- DellCommandInstall.ps1
+++ DellCommandInstall.ps1
@@ -1,4 +1,6 @@
-﻿Write-Host 'Downloading Dell Command Update Installer...'
+﻿### Blank Space ####
+
+Write-Host 'Downloading Dell Command Update Installer...'
 
 $headers = @{
   'User-Agent' = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'
