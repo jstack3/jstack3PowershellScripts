339678e57a3bb03b9ac09c07d659becdcc512014
jstack3 <141942773+jstack3@users.noreply.github.com>
2024-02-14 11:59:27 -0500

    Update InstallmsixmgrGPO.ps1

--- InstallmsixmgrGPO.ps1
+++ InstallmsixmgrGPO.ps1
@@ -1,21 +1,2 @@
-$ProgressPreference = 'SilentlyContinue'
 
-Invoke-WebRequest -Uri "https://download.microsoft.com/download/d/3/5/d35c060c-d402-4a9c-ad40-234ad5e4d2e8/DesktopAppInstallerPolicies.zip" -OutFile "$env:TEMP\msixmgrGPO.zip"
-
-Expand-Archive -Path "$env:TEMP\msixmgrGPO.zip" -DestinationPath "$env:TEMP\msixmgrGPO" -Force
-
-Copy-Item  -Path "$env:TEMP\msixmgrGPO\admx\en-US\DesktopAppInstaller.adml" -Destination "C:\Windows\PolicyDefinitions\en-US\."
-
-Copy-Item  -Path "$env:TEMP\msixmgrGPO\admx\DesktopAppInstaller.admx" -Destination "C:\Windows\PolicyDefinitions\."
-
-if (Test-Path "$env:TEMP\msixmgrGPO.zip"){
-
-Remove-Item "$env:TEMP\msixmgrGPO.zip" -Force -ErrorAction SilentlyContinue
-
-}
-
-if (Test-Path "$env:TEMP\msixmgrGPO"){
-
-Remove-Item "$env:TEMP\msixmgrGPO" -Force -ErrorAction SilentlyContinue -Recurse
-
-}
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\AppInstaller" -Name "EnableMSAppInstallerProtocol" -Value 1
