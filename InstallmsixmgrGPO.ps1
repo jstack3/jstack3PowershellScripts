73a1a2aa882c4773d0b0b60c3b1b061a5c39d2f5
jstack3 <141942773+jstack3@users.noreply.github.com>
2024-02-14 10:47:14 -0500

    Update InstallmsixmgrGPO.ps1

--- InstallmsixmgrGPO.ps1
+++ InstallmsixmgrGPO.ps1
@@ -2,6 +2,6 @@ $ProgressPreference = 'SilentlyContinue'
 
 Invoke-WebRequest -Uri "https://download.microsoft.com/download/d/3/5/d35c060c-d402-4a9c-ad40-234ad5e4d2e8/DesktopAppInstallerPolicies.zip" -OutFile "$env:TEMP\msixmgrGPO.zip"
 
-Expand-Archive -LiteralPath '$env:TEMP\msixmgrGPO.zip' -DestinationPath '$env:TEMP\msixmgrGPO' -Force
+Expand-Archive -Path "$env:TEMP\msixmgrGPO.zip" -DestinationPath "$env:TEMP\msixmgrGPO" -Force
 
-Copy-Item -LiteralPath '$env:TEMP\msixmgrGPO\admx\en-US\DesktopAppInstaller.adml' -Destination 'C:\Windows\PolicyDefinitions\en-US\.'
+Copy-Item  -Path "$env:TEMP\msixmgrGPO\admx\en-US\DesktopAppInstaller.adml" -Destination "C:\Windows\PolicyDefinitions\en-US\."
