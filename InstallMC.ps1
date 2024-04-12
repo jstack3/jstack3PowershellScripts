5c5a8e0255b4eb3c183153a57357dc0b7caea3bf
jstack3 <141942773+jstack3@users.noreply.github.com>
2024-04-12 10:49:04 -0400

    Create InstallMC.ps1
    MC Education Install Script

---
+++ InstallMC.ps1
@@ -0,0 +1,23 @@
+﻿
+ ### Blank Space ####
+
+ $ProgressPreference = 'SilentlyContinue'
+
+ $PowershellPrompt = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+
+if (!($PowershellPrompt.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))) {
+    Write-Host -ForegroundColor Red "Please run this script as Admin! Press any key to quit!"
+    Read-Host
+    exit
+}
+
+Write-Host "Downloading Minecraft Education Installer..."
+
+Invoke-WebRequest -Uri "https://aka.ms/downloadmee-desktopApp" -OutFile "$env:MinecraftEducationInstall.exe"
+
+Write-Host "Installing Minecraft Education..."
+
+Start-Process cmd.exe -Verb runAs -ArgumentList "/c $env:MinecraftEducationInstall.exe /exenoui /i /qn" -Wait
+
+Write-Host 'Sleeping for 30 seconds while Minecraft Installs...'
+Start-Sleep -Seconds 30
