b93b925bb5754037bf072020def93c147c6d64db
jstack3 <141942773+jstack3@users.noreply.github.com>
2024-05-03 11:01:02 -0400

    Update DellCommandInstall.ps1

--- DellCommandInstall.ps1
+++ DellCommandInstall.ps1
@@ -18,9 +18,9 @@ $headers = @{
   'User-Agent' = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'
 }
 
-Invoke-WebRequest -Uri "https://dl.dell.com/FOLDER11201586M/1/Dell-Command-Update-Windows-Universal-Application_0XNVX_WIN_5.2.0_A00.EXE" -OutFile "C:\Users\Public\Dell-Command-Update-Windows-Universal-Application_0XNVX_WIN_5.2.0_A00.EXE" -Headers $headers
+Invoke-WebRequest -Uri "https://dl.dell.com/FOLDER11563484M/1/Dell-Command-Update-Windows-Universal-Application_P83K5_WIN_5.3.0_A00.EXE" -OutFile "C:\Users\Public\Dell-Command-Update-Windows-Universal-Application_P83K5_WIN_5.3.0_A00.EXE" -Headers $headers
 Write-Host 'Extracting Dell Command Update Installer...'
-Start-Process cmd.exe -Verb runAs -ArgumentList "/c C:\Users\Public\Dell-Command-Update-Windows-Universal-Application_0XNVX_WIN_5.2.0_A00.EXE /s /e=C:\Users\Public\DellCommandInstall" -Wait
+Start-Process cmd.exe -Verb runAs -ArgumentList "/c C:\Users\Public\Dell-Command-Update-Windows-Universal-Application_P83K5_WIN_5.3.0_A00.EXE /s /e=C:\Users\Public\DellCommandInstall" -Wait
 Write-Host 'Installing Dell Command Update...'
 Start-Process cmd.exe -Verb runAs -ArgumentList "/c C:\Users\Public\DellCommandInstall\DellCommandUpdateApp_Setup.exe /S /v/qn" -Wait
 
@@ -29,7 +29,7 @@ Write-Host 'Sleeping for 30 seconds while Dell Command Update Installs...'
 Start-Sleep -Seconds 30
 
 Write-Host 'Removing Installer...'
-Remove-Item C:\Users\Public\Dell-Command-Update-Windows-Universal-Application_0XNVX_WIN_5.2.0_A00.EXE
+Remove-Item C:\Users\Public\Dell-Command-Update-Windows-Universal-Application_P83K5_WIN_5.3.0_A00.EXE
 Remove-Item C:\Users\Public\DellCommandInstall -Recurse
 
 Write-Host "Press any key to restart the computer."
