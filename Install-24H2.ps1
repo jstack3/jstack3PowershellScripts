06cd28dd04564728f10d3a5e4cf3c9f651c19d6c
jstack3 <141942773+jstack3@users.noreply.github.com>
2025-03-20 15:51:44 -0400

    Create Install-24H2.ps1

---
+++ Install-24H2.ps1
@@ -0,0 +1,3 @@
+Invoke-webrequest -uri https://go.microsoft.com/fwlink/?linkid=2171764 -outfile $env:TEMP\Updater.exe -UseBasicParsing 
+
+$env:TEMP\Updater.exe /skipeula /auto clean /DynamicUpdate Enable 
