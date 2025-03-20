5d2ff5b8c514bbc41084e07e8cbba2a77bb07441
jstack3 <141942773+jstack3@users.noreply.github.com>
2025-03-20 15:58:51 -0400

    Update Install-24H2.ps1

--- Install-24H2.ps1
+++ Install-24H2.ps1
@@ -3,4 +3,4 @@ $ProgressPreference = 'silentlycontinue'
 
 Invoke-webrequest -uri https://go.microsoft.com/fwlink/?linkid=2171764 -outfile $env:TEMP\Updater.exe -UseBasicParsing 
 
-$env:TEMP\Updater.exe /skipeula /auto clean /DynamicUpdate Enable 
+& $env:TEMP\Updater.exe /skipeula /auto clean /DynamicUpdate Enable 
