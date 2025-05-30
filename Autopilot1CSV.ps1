1ecb470f1726d8bec56357747a43296bc4fef34e
jstack3 <141942773+jstack3@users.noreply.github.com>
2025-05-30 09:33:16 -0400

    Create Autopilot1CSV.ps1

---
+++ Autopilot1CSV.ps1
@@ -0,0 +1,39 @@
+﻿param (
+    [Parameter(Mandatory=$true)]
+    [string]$folderpath
+)
+
+if (!(Test-Path $folderpath)){
+
+Write-Host -ForegroundColor Red "Invalid folder! Try again!"
+Read-Host
+exit
+
+}
+
+
+$data = @()
+
+$newfile = $folderPath + "\all.csv"
+
+if (Test-Path $newfile){
+
+Remove-Item -Force $newfile
+
+}
+
+
+$files = Get-ChildItem -Path $folderPath -File
+
+foreach ($file in $files) {
+
+$filepath = $folderPath + "\" + $file
+
+
+$data += Import-Csv -Path $filepath -ErrorAction Stop
+
+
+
+}
+
+$data | Export-Csv -Path $newfile -Force -NoTypeInformation
