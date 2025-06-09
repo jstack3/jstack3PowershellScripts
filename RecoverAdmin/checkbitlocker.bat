fa361bb5662b19c858d6600b715f2b2af2455a08
jstack3 <141942773+jstack3@users.noreply.github.com>
2025-06-09 15:29:17 -0400

    Update checkbitlocker.bat
    error checking doesnt work

--- RecoverAdmin/checkbitlocker.bat
+++ RecoverAdmin/checkbitlocker.bat
@@ -3,17 +3,27 @@ for /f "tokens=3" %%A in ('manage-bde -status C: ^| find "Lock Status"') do (
     if "%%A"=="Locked" (
         echo Selected Drive is bitlocked. 
 
-		choice /c YN /n /m "Do you have the key? (Y/N) "
-
-		if errorlevel 2 (
-			echo exiting...
-			exit
-		) else (
-			echo Not locked. Continuing...
+		set /p key_check="Do you have the key (Y/n)? "
+		
+		if NOT "%key_check%"=="Y" (
+		echo Going to end!
+		goto end
 		)
+		
+		set /p recovery_key="Type in the key (with dashes): "
+		
+		manage-bde -unlock C: -RecoveryPassword %recovery_key%
+		
+		if ERRORLEVEL 1 (
+			echo Error has occured with unlock!!!
+			goto end
+		)
+			
+		
     ) else (
         echo Selected Drive is not bitlocked.
     )
 )
 
+:end
 echo End of script!!!
