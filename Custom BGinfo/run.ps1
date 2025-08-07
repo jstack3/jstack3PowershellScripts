01d3a0ff65406600e056c1a80a5a9d379c7d4360
jstack3 <141942773+jstack3@users.noreply.github.com>
2025-08-07 17:27:45 -0400

    BGinfo
    script to print MAC/IP address to desktop. Also includes .exe. Utilizes bginfo from sysinternals.

---
+++ Custom BGinfo/run.ps1
@@ -0,0 +1,25 @@
+﻿if (Test-Path $env:TEMP\bginfoNETWORK.txt){
+
+Remove-Item $env:TEMP\bginfoNETWORK.txt
+
+}
+
+
+
+$interfaces = Get-NetAdapter -Physical | Where-Object InterfaceType -eq 6
+
+foreach ($interface in $interfaces) {
+
+$ipaddress = Get-NetIPAddress -InterfaceAlias $interface.InterfaceAlias | Select-Object -ExpandProperty IPv4Address
+
+$macaddress = $interface.MacAddress
+
+$interfaceDescription = $interface.InterfaceDescription
+
+$interfaceDescription + " -- " + $ipaddress + " -- " + $macaddress | Add-Content -Path "$env:TEMP\bginfoNETWORK.txt"
+
+}
+
+Expand-Archive BGInfo.zip -DestinationPath .\BGInfo\
+
+Start-Process -Wait .\BGInfo\Bginfo.exe -ArgumentList "MACinfo.bgi", "/silent", "/timer:0", "/accepteula"
